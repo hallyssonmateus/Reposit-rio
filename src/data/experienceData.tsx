036484caede543c95bf6d4223b06214ac2efd5f5
Hallysson Mateus <hallysson_gato@hotmail.com>
2025-09-09 00:08:43 -0300

    Feat: Adicionando as informações no campo das experiencias

--- src/data/experienceData.tsx
+++ src/data/experienceData.tsx
@@ -4,13 +4,32 @@ export const experienceData = [
     company: 'Télos conecta',
     period: 'jul/2024 - fev/2025',
     location: 'São Paulo, SP',
-    description: 'Liderança técnica no desenvolvimento de aplicações web enterprise utilizando .NET Core e React. Responsável pela arquitetura de soluções e mentoria da equipe junior.',
+    description: '',
     achievements: [
-      'Reduziu o tempo de carregamento das aplicações em 40%',
-      'Implementou arquitetura de microsserviços que aumentou a escalabilidade',
-      'Liderou equipe de 4 desenvolvedores junior'
+      'Realizei análise, projeto e desenvolvimento de aplicações web utilizando a linguagem C# e os frameworks .NET Framework/Core.',
+      'Colaborei com a equipe para projetar e implementar soluções eficientes, com destaque para a arquitetura de APIs RESTful.',
+      'Integrei sistemas com banco de dados SQL Server, modelando o banco de dados e escrevendo consultas otimizadas para garantir a integridade dos dados.',
+      'Participei de reuniões técnicas para levantamento de requisitos e discussão de melhorias em projetos existentes.',
+      'Realizei testes unitários (xUnit/NUnit) e depuração de código para garantir a qualidade das entregas.',
+      'Utilizei GIT/GitHub para controle de versão em todos os projetos.'
     ],
-    technologies: ['C#', '.NET Core', 'React', 'SQL Server', 'Azure']
+    technologies: ['C#', '.NET Core', 'Entity Framework', 'React', 'SQL Server','Api REST', 'xUnit']
   },
+  {
+    title: 'Desenvolvedor Frontend Web Jr',
+    company: 'Proz Educação',
+    period: 'abr/2024 - out/2024',
+    location: 'São Paulo, SP',
+    description: '',
+    achievements: [
+      'Colaborei no desenvolvimento de um projeto em equipe, utilizando HTML5, CSS3, e JavaScript em diversas funcionalidades.',
+      'Apliquei conceitos de banco de dados relacional e boas práticas de desenvolvimento.',
+      'Contribuí ativamente na revisão de código e na resolução de bugs, compartilhando conhecimentos e experiências com os colegas.',
+      'Participei ativamente de rituais ágeis (Scrum/Kanban) e utilizei Git/GitHub para o desenvolvimento colaborativo.',
+      'Realizei testes unitários (xUnit/NUnit) e depuração de código para garantir a qualidade das entregas.',
+      'Utilizei GIT/GitHub para controle de versão em todos os projetos.'
+    ],
+    technologies: ['HTML5', 'CSS3', 'JavaScript', 'Figma', 'Scrum','Kanban','Python','MySQL']
+  }
 
-]
+];
