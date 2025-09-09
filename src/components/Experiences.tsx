b2dd042cdc54b975bd767e8767b7bbdcc8d365fc
Hallysson Mateus <hallysson_gato@hotmail.com>
2025-09-08 23:36:32 -0300

    Feat: Criação da seção Experiencia Profissional

---
+++ src/components/Experiences.tsx
@@ -0,0 +1,13 @@
+
+function Experiences (){
+    return (
+        <section className="bg-gray-100 py-16">
+            <div>
+                <h2>Experiencia Profissional</h2>
+                <p>Minha trajetória profissional e evolução técnica ao longos dos anos</p>
+            </div>
+        </section>
+    )
+}
+
+export default Experiences
