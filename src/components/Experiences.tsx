f60d134aa20f25512a69206c789349a37b7abfb7
Hallysson Mateus <hallysson_gato@hotmail.com>
2025-09-12 14:20:58 -0300

    Feat: Ajustando fontes e tamanho do titulo e subtitulo; Adicionando Padding inferior para melhor espaçamento

--- src/components/Experiences.tsx
+++ src/components/Experiences.tsx
@@ -4,11 +4,11 @@ import ExperienceItem from "./ExperienceItem"
 
 function Experiences (){
     return (
-        <section className="bg-gray-100 py-16">
+        <section id="experience" className="bg-gray-100 py-16">
             <div className="container mx-auto text-center px-4">
                 <div>
-                    <h2 className="text-3x1 text-gray-600 font-bold mb-2">Experiência Profissional</h2>
-                    <p className="text-gray-600">Minha trajetória profissional e evolução técnica ao longo dos anos</p>
+                    <h2 className="text-3xl text-gray-600 text-bold text-center mb-4">Experiência Profissional</h2>
+                    <p className="text-gray-600 text-center mb-12">Minha trajetória profissional e evolução técnica ao longo dos anos</p>
                 </div>
                 
                 <div className="relative">
