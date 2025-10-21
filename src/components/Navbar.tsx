0b17ed2a56931a0d20b63ac8a63100b7cf1462b6
Hallysson Mateus <hallysson_gato@hotmail.com>
2025-10-21 01:56:07 -0300

    refactor: Ajustes no padding e navbar itens na responsividade no Navbar.tsx; Adicionando um logotipo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,20 +14,30 @@ function Navbar() {
   };
 
   return (
-    <header className="bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-gray-100">
+    <header className="bg-gray-200 dark:bg-gray-800 text-gray-800 dark:text-gray-100">
       <div className="flex justify-between items-center py-6 max-w-5xl mx-auto px-4">
-        <h1 className="text-2xl font-bold tracking-tight text-gray-800 dark:text-gray-100">Hallysson Mateus</h1>
-        <nav className="flex items-center space-x-6">
+        <div className="relative flex items-center gap-2 px-2">
+          <a href="#" className="bg-blue-600 relative text-white rounded-xl px-2 py-1 font-semibold">
+            <span className="relative z-10">{"</>"}</span>
+            <span className="absolute inset-0 bg-gradient-to-t from-black/40 to-transparent rounded-xl"></span>
+          </a>
+          <h1 className="text-2xl font-bold tracking-tight text-gray-800 dark:text-gray-100">Hallysson Mateus</h1>
+        </div>
+        
+        <nav className="hidden md:flex items-center space-x-6">
           <a href="#about" className="hover:text-blue-600 dark:hover:text-blue-400 transition">Sobre</a>
           <a href="#skills" className="hover:text-blue-600 dark:hover:text-blue-400 transition">Skills</a>
           <a href="#projects" className="hover:text-blue-600 dark:hover:text-blue-400 transition">Projetos</a>
           <a href="#experience" className="hover:text-blue-600 dark:hover:text-blue-400 transition">Experiência</a>
-          <a href="#contact" className="hover:text-blue-600 dark:hover:text-blue-400 transition">Contato</a>
+          <a href="#contact" className="hover:text-blue-600 dark:hover:text-blue-400 transition">Contato</a> 
 
-          <button onClick={toggleDarkMode} className="ml-4 p-2 rounded-md hover:bg-blue-600 dark:hover:bg-gray-700 transition">
+          <button onClick={toggleDarkMode} className="ml-4 p-2 block rounded-md hover:bg-blue-600 dark:hover:bg-gray-700 transition">
             {darkMode ? (<SunIcon className="w-3 h-3 text-white-200" />) : (<MoonIcon className="w-3 h-3 text-white-200" />)}
           </button>
         </nav>
+        <button className="md:hidden p-2 rounded-md text-zinc-800 hover:bg-gray-300 dark:hover:bg-gray-700 dark:text-white transition">
+            ☰
+          </button> 
       </div>
     </header>
 
